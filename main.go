e859df628e27632a7a1c8c5892e9c8d230a87cc2
z <z@host>
2017-02-11 22:26:26 +0800

    add log middleware

--- main.go
+++ main.go
@@ -6,9 +6,17 @@ import (
 	"os"
 )
 
+// Manning go web programming - page:83
+func log(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		h.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	dir, _ := os.Getwd()
-	http.Handle("/", http.FileServer(http.Dir(dir)))
+	//http.Handle("/", http.FileServer(http.Dir(dir)))
 
 	var port string
 	arg := os.Args
@@ -18,10 +26,15 @@ func main() {
 		port = ":" + arg[1]
 	}
 
-	fmt.Printf("From: %s\nListening localhost%s", dir, port)
+	fmt.Printf("From: %s\nListening localhost%s\n", dir, port)
+
 	server := http.Server{
 		Addr: port,
 	}
+
+	fs := http.FileServer(http.Dir(dir))
+	http.Handle("/", log(fs))
+
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
